6b2ff9354b8f6a5d0bb82f09a5688fe9a147a9e7
WollfZera <95146984+WollfZera@users.noreply.github.com>
2021-11-30 21:34:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var names[i] = console.log(prompt('Input'))
 }
 
 for (var i = 0; i < names.length; i++) {
-  if (i == 0)
-	firstLetter = names[i]
   if (firstLetter == 'j') {
 	byeSpeaker.speak(names[i]);
   } else {
